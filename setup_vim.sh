133184a2cb6319aafaf0a6de5ad89c2f9bc97b80
a333klm <j_egelhof16@cs.uni-kl.de>
2021-11-25 22:55:55 +0100

    Create setup_vim.sh

---
+++ setup_vim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p ~/.vim/pack/plugins/start/
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/
+git clone https://github.com/easymotion/vim-easymotion.git ~/.vim/pack/plugins/start/
+
+echo "nnoremap <SPACE> <Nop>" 
+let mapleader=" "
+
+cat <<EOT >> ~/.vimrc
+let g:ctrlp_map = '<c-p>'
+let g:ctrlp_cmd = 'CtrlP'
+map  <Leader>f <Plug>(easymotion-bd-f)
+nmap <Leader>f <Plug>(easymotion-overwin-f)
+
+" Move to line
+map <Leader>l <Plug>(easymotion-bd-jk)
+nmap <Leader>l <Plug>(easymotion-overwin-line)
+
+" Move to word
+map  <Leader>w <Plug>(easymotion-bd-w)
+nmap <Leader>w <Plug>(easymotion-overwin-w)
+EOT
