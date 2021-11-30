b565629d9761a10b57fb0dfc350ddbab66de93f7
a333klm <j_egelhof16@cs.uni-kl.de>
2021-11-30 16:48:28 +0100

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.vim/pack/plugins/start/
-git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlpvim
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/plugins/start/gruvbox
-
+git clone https://github.com/junegunn/fzf.git ~/.vim/pack/packages/start/fzf
+git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/packages/start/fzf.vim
 mv .vimrc ~/.vimrc
