015bb022844a2b289c83a1f07f4bae1ecc6b35bf
a333klm <j_egelhof16@cs.uni-kl.de>
2021-11-30 16:02:43 +0100

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.vim/pack/plugins/start/
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlpvim
-git clone https://github.com/easymotion/vim-easymotion.git ~/.vim/pack/plugins/start/vim-easymotion
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/plugins/start/gruvbox
 
 mv .vimrc ~/.vimrc
