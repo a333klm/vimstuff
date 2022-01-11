4024cb0b1b7f373c3e9ca858614db602809ca501
a333klm <j_egelhof16@cs.uni-kl.de>
2022-01-11 09:13:48 +0100

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p ~/.vim/pack/plugins/start/
 mkdir -p ~/.vim/colors/
+wget https://gist.githubusercontent.com/bfrg/dc149d1b797dd65502db38b1d76b5edd/raw/f75376c5affa38f3745ee0bf23ec2092a9aa757e/onedark.vim
 mv onedark.vim ~/.vim/colors/
 git clone https://github.com/junegunn/fzf.git ~/.vim/pack/plugins/start/fzf
 git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/plugins/start/fzf.vim
-git clone https://github.com/doums/darcula.git ~/.vim/pack/plugins/start/darcula
 git clone https://github.com/ludovicchabant/vim-lawrencium.git ~/.vim/pack/plugins/start/vim-lawrencium
 git clone https://github.com/bfrg/vim-cpp-modern.git ~/.vim/pack/plugins/start/vim-cpp-modern
 mv .vimrc ~/.vimrc
